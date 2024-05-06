3878de26a928464d25f0ad6174c5f0e6712021e3
Hemdipv <hemdipv.techup@gmail.com>
2024-05-06 17:34:50 +0530

    changes for roles

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -65,38 +65,7 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('/addUser', async (req, res) => {
-    const { firstName, lastName, age, gender } = req.body;
 
-    // Validate input fields
-    if (!firstName || !lastName || !age || !gender) {
-        return res.status(400).json({ error: 'First name and last name are required' });
-    }
-
-    // Generate a unique ID for the user
-    const userId = uuidv4();
-
-    // Create user object with ID, first name, and last name
-    const user = { id: userId, firstName, lastName, age, gender };
-
-    // Connect to MongoDB
-    const client = new MongoClient('mongodb+srv://vhemdip:AMv69NI2cCSwaI9Y@cluster0.hivu7de.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
-        await client.connect();
-        const db = client.db('vhemdip'); // Replace 'yourDatabaseName' with your actual database name
-        const collection = db.collection('add_user'); // Assuming your collection name is 'user'
-
-        // Insert new user into MongoDB
-        await collection.insertOne(user);
-
-        res.status(201).json({ message: 'User added successfully', user });
-    } catch (error) {
-        console.error('Error adding user:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    } finally {
-        await client.close();
-    }
-});
 
 
 
@@ -174,7 +143,38 @@ router.get('/mobile-list', async (req, res) => {
 
 
 
+router.post('/addUser', async (req, res) => {
+    const { firstName, lastName, age, gender } = req.body;
 
+    // Validate input fields
+    if (!firstName || !lastName || !age || !gender) {
+        return res.status(400).json({ error: 'First name and last name are required' });
+    }
+
+    // Generate a unique ID for the user
+    const userId = uuidv4();
+
+    // Create user object with ID, first name, and last name
+    const user = { id: userId, firstName, lastName, age, gender };
+
+    // Connect to MongoDB
+    const client = new MongoClient('mongodb+srv://vhemdip:AMv69NI2cCSwaI9Y@cluster0.hivu7de.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        const db = client.db('vhemdip'); // Replace 'yourDatabaseName' with your actual database name
+        const collection = db.collection('add_user'); // Assuming your collection name is 'user'
+
+        // Insert new user into MongoDB
+        await collection.insertOne(user);
+
+        res.status(201).json({ message: 'User added successfully', user });
+    } catch (error) {
+        console.error('Error adding user:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
 
 router.delete('/delete-user/:userId', async (req, res) => {
     const userId = req.params.userId;
@@ -238,33 +238,6 @@ router.put('/editUser/:userId', async (req, res) => {
     }
 });
 
-
-router.get('/user-details/:userId', async (req, res) => {
-    const userId = req.params.userId;
-
-    // Connect to MongoDB
-    const client = new MongoClient('mongodb+srv://vhemdip:AMv69NI2cCSwaI9Y@cluster0.hivu7de.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
-        await client.connect();
-        const db = client.db('vhemdip'); // Replace 'yourDatabaseName' with your actual database name
-        const collection = db.collection('add_user'); // Assuming your collection name is 'user'
-
-        // Fetch user details from MongoDB
-        const user = await collection.findOne({ id: userId });
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
-        res.status(200).json({ user });
-    } catch (error) {
-        console.error('Error fetching user:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    } finally {
-        await client.close();
-    }
-});
-
-
 router.get('/users', async (req, res) => {
     const { firstName, page, pageSize = 10 } = req.query; // Get the search query and pagination parameters
 
@@ -304,5 +277,175 @@ router.get('/users', async (req, res) => {
         await client.close();
     }
 });
+
+router.get('/user-details/:userId', async (req, res) => {
+    const userId = req.params.userId;
+
+    // Connect to MongoDB
+    const client = new MongoClient('mongodb+srv://vhemdip:AMv69NI2cCSwaI9Y@cluster0.hivu7de.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        const db = client.db('vhemdip'); // Replace 'yourDatabaseName' with your actual database name
+        const collection = db.collection('add_user'); // Assuming your collection name is 'user'
+
+        // Fetch user details from MongoDB
+        const user = await collection.findOne({ id: userId });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json({ user });
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+
+
+
+
+
+
+router.post('/addRole', async (req, res) => {
+    const { roleName} = req.body;
+
+    // Validate input fields
+    if (!roleName) {
+        return res.status(400).json({ error: 'Role name are required' });
+    }
+
+    // Generate a unique ID for the user
+    const userId = uuidv4();
+
+    // Create user object with ID, first name, and last name
+    const user = { id: roleId, roleName };
+
+    // Connect to MongoDB
+    const client = new MongoClient('mongodb+srv://vhemdip:AMv69NI2cCSwaI9Y@cluster0.hivu7de.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        const db = client.db('vhemdip'); // Replace 'yourDatabaseName' with your actual database name
+        const collection = db.collection('user_roles'); // Assuming your collection name is 'user'
+
+        // Insert new user into MongoDB
+        await collection.insertOne(user);
+
+        res.status(201).json({ message: 'User Role added successfully', user });
+    } catch (error) {
+        console.error('Error adding user:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+router.delete('/delete-role/:roleId', async (req, res) => {
+    const userId = req.params.userId;
+
+    // Connect to MongoDB
+    const client = new MongoClient('mongodb+srv://vhemdip:AMv69NI2cCSwaI9Y@cluster0.hivu7de.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        const db = client.db('vhemdip'); // Replace 'yourDatabaseName' with your actual database name
+        const collection = db.collection('add_user'); // Assuming your collection name is 'user'
+
+        // Check if user with given ID exists
+        const existingUser = await collection.findOne({ id: userId });
+        if (!existingUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Delete user from MongoDB
+        await collection.deleteOne({ id: userId });
+
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+router.put('/editRole/:roleId', async (req, res) => {
+    const userId = req.params.userId;
+    const { firstName, lastName, age, gender } = req.body;
+
+    // Validate input fields
+    if (!firstName || !lastName || !age || !gender) {
+        return res.status(400).json({ error: 'First name and last name are required' });
+    }
+
+    // Connect to MongoDB
+    const client = new MongoClient('mongodb+srv://vhemdip:AMv69NI2cCSwaI9Y@cluster0.hivu7de.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        const db = client.db('vhemdip'); // Replace 'yourDatabaseName' with your actual database name
+        const collection = db.collection('add_user'); // Assuming your collection name is 'user'
+
+        // Check if user with given ID exists
+        const existingUser = await collection.findOne({ id: userId });
+        if (!existingUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Update user details in MongoDB
+        await collection.updateOne({ id: userId }, { $set: { firstName, lastName, age, gender } });
+
+        res.status(200).json({ message: 'User updated successfully' });
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+router.get('/roles', async (req, res) => {
+    const { firstName, page, pageSize = 10 } = req.query; // Get the search query and pagination parameters
+
+    // Convert page and pageSize to numbers
+    const pageNumber = page ? parseInt(page) : null;
+    const limit = parseInt(pageSize);
+
+    const client = new MongoClient('mongodb+srv://vhemdip:AMv69NI2cCSwaI9Y@cluster0.hivu7de.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        const db = client.db('vhemdip');
+        const collection = db.collection('user_roles');
+
+        let query = {};
+        if (firstName) {
+            const regex = new RegExp(firstName, 'i'); 
+            query.firstName = regex;
+        }
+
+        if (pageNumber !== null) {
+            const skip = (pageNumber - 1) * limit;
+
+            const totalCount = await collection.countDocuments(query);
+
+            const users = await collection.find(query).skip(skip).limit(limit).toArray();
+
+            res.status(200).json({ totalCount, users });
+        } else {
+            const users = await collection.find(query).toArray();
+
+            res.status(200).json({ totalCount: users.length, users });
+        }
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+
+
+
 
 module.exports = router;
